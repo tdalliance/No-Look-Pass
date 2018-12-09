5f6fd3877968b9af79cfc9ba8121112892f77a2e
Steven Oh <stevenoh0908@gmail.com>
2018-12-09 21:28:14 +0900

    v1.0
    정식으로 사운드 업데이트하고, 불필요한 이미지를 제거하여 소스 정식 버전을 커밋합니다.
    
    #추가
    - sound\background.wav -> 배경 음악
    - sound\button-press.wav -> a, d키 눌러 가방 스폰할 때 재생
    - sound\hit.wav -> 체력이 깎일 때 재생
    - sound\start.wav -> 시작할 때 재생
    
    #제거
    - image\ass_car_left.png
    - image\ass_car_right.png
    - 두 이미지 모두 불필요하여 제거함.
    
    #수정
    - 제거된 이미지 로드 부분 제거
    - 사운드 핸들링 추가

--- no_look_pass.py
+++ no_look_pass.py
@@ -40,6 +40,7 @@ def drawText(text, size, color, x, y, align):
 def initGame():
     global gamepad, clock
     global background, mr, ass, car
+    global start_sound, car_sound, crash_sound
 
     mr = []
     ass = []
@@ -58,11 +59,13 @@ def initGame():
 
     ass.append(pygame.image.load('image/ass_left.png'))
     ass.append(pygame.image.load('image/ass_right.png'))
-    ass.append(pygame.image.load('image/ass_car_left.png'))
-    ass.append(pygame.image.load('image/ass_car_right.png'))
 
     car.append(pygame.image.load('image/car_left.png'))
     car.append(pygame.image.load('image/car_right.png'))
+
+    start_sound = pygame.mixer.Sound('sound/start.wav')
+    car_sound = pygame.mixer.Sound('sound/button-press.wav')
+    crash_sound = pygame.mixer.Sound('sound/hit.wav')
 
     clock = pygame.time.Clock()
     mainScreen()
@@ -70,6 +73,7 @@ def initGame():
 
 def mainScreen():
     global gamepad, background
+    global start_sound
 
     i = 1
     msgDisp = False
@@ -78,6 +82,7 @@ def mainScreen():
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
+                    pygame.mixer.Sound.play(start_sound)
                     runGame()
                     pass
                 pass
@@ -110,11 +115,45 @@ def mainScreen():
 
 def gameOver():
     global gamepad
+    drawText("게임 오버", 100, color.black, pad_width / 2, pad_height / 2 - 80, center)
+    drawText("언론의 보도로 이미지가 실추되었습니다!", 30, color.black, pad_width / 2, pad_height / 2 -30, center)
+    drawText("다시 하시겠습니까? (Y/N)", 50, color.black, pad_width / 2, pad_height / 2 + 40, center)
+    pygame.display.update()
+    crashed = False
+    while not crashed:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_y:
+                    runGame()
+                    pass
+                elif event.key == pygame.K_n:
+                    pygame.quit()
+                    quit()
+                    pass
+                pass
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                pass
+            pass
+        pass
     pass
 
 def runGame():
     global gamepad, clock
     global background, mr, ass, car
+    global car_sound, crash_sound
+
+    pygame.mixer.music.load('sound/background.wav')
+    pygame.mixer.music.play(-1)
+
+    life = 3
+
+    left_ass_cnt = 0
+    right_ass_cnt = 0
+
+    left_car_cnt = 0
+    right_car_cnt = 0
 
     ass_txy = []
     car_txy = []
@@ -131,17 +170,32 @@ def runGame():
 
     crashed = False
     while not crashed:
+        if life < 0:
+            gameOver()
+            pass
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
+                    pygame.mixer.Sound.play(car_sound)
                     mr_t = 1
                     mr_t_cooltime = 0
                     car_txy.append([car[0], mr_x - 20, mr_y + 210])
+                    left_car_cnt += 1
+                    if left_car_cnt > left_ass_cnt:
+                        pygame.mixer.Sound.play(crash_sound)
+                        life -= 1
+                        pass
                     pass
                 elif event.key == pygame.K_d:
+                    pygame.mixer.Sound.play(car_sound)
                     mr_t = 2
                     mr_t_cooltime = 0
                     car_txy.append([car[1], mr_x + 120, mr_y + 210])
+                    right_car_cnt += 1
+                    if right_car_cnt > right_ass_cnt:
+                        pygame.mixer.Sound.play(crash_sound)
+                        life -= 1
+                        pass
                     pass
                 pass
             if event.type == pygame.QUIT:
@@ -149,15 +203,17 @@ def runGame():
                 pass
             pass
 
-        speed = 3 + count * 0.05
+        speed = 3 + count * 0.3
 
         ass_spawn = random.randrange(0, int(700/speed))
 
         if ass_spawn == 1:
             ass_txy.append([ass[0], -100, mr_y + 50, 0])
+            left_ass_cnt += 1
             pass
         elif ass_spawn == 2:
             ass_txy.append([ass[1], pad_width, mr_y + 50, 0])
+            right_ass_cnt += 1
             pass
 
         if not len(ass_txy) == 0:
@@ -165,22 +221,54 @@ def runGame():
                 if a[0] == ass[0]:
                     a[1] += speed
                     ass_txy[i][1] = a[1]
-                    if a[1] > pad_width:
+                    if a[1] > mr_x:
                         try:
                             ass_txy.remove(a)
                             pass
                         except:
                             pass
+                        left_ass_cnt -= 1
+                        pygame.mixer.Sound.play(crash_sound)
+                        life -= 1
                         pass
                     pass
                 elif a[0] == ass[1]:
                     a[1] -= speed
                     ass_txy[i][1] = a[1]
-                    if a[1] < -100:
+                    if a[1] < mr_x:
                         try:
                             ass_txy.remove(a)
                             pass
                         except:
+                            pass
+                        right_ass_cnt -= 1
+                        pygame.mixer.Sound.play(crash_sound)
+                        life -= 1
+                        pass
+                    pass
+                if not len(car_txy) == 0:
+                    for i, c in enumerate(car_txy):
+                        if a[0] == ass[0] and c[0] == car[0] and a[1] > c[1] - 80:
+                            score += int(speed)
+                            count += 1
+                            try:
+                                car_txy.remove(c)
+                                ass_txy.remove(a)
+                                pass
+                            except:
+                                pass
+                            left_car_cnt -= 1
+                            pass
+                        elif a[0] == ass[1] and c[0] == car[1] and a[1] < c[1] + 40:
+                            score += int(speed)
+                            count += 1
+                            try:
+                                car_txy.remove(c)
+                                ass_txy.remove(a)
+                                pass
+                            except:
+                                pass
+                            right_car_cnt -= 1
                             pass
                         pass
                     pass
@@ -239,6 +327,9 @@ def runGame():
 
         
         drawObject(mr[mr_t], mr_x, mr_y)
+
+        drawText("Life : " + str(life), 50, color.black, 20, 30, left)
+        drawText("Score : " + str(score), 50, color.black, pad_width - 20, 30, right)
         pygame.display.update()
         clock.tick(60)
         pass
